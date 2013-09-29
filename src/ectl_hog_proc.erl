acdd3cd38965bc89f152853817a682a1803013dc
stwind <stwindfy@gmail.com>
2013-09-29 09:57:43 +0800

    Prettify proc_hog command

--- src/ectl_hog_proc.erl
+++ src/ectl_hog_proc.erl
@@ -9,11 +9,31 @@
 %% ===================================================================
 
 run(Opts) ->
-    Node = ectl_lib:arg(node, Opts),
-    Cookie = ectl_lib:arg(cookie, Opts),
-    {ok, _} = ecli:connect_node(Node, Cookie), 
+    ecli:start_node(ectl_lib:arg(cookie, Opts)),
+    Node = list_to_atom(ecli:binding(node, Opts)),
     ectl_lib:load_recon(Node),
     Field = list_to_atom(ecli:binding(attr, Opts)),
     Num = ecli:opt(num, Opts),
     Info = rpc:call(Node, recon, proc_count, [Field, Num]),
-    ?PRINT("~p~n",[Info]).
+    print_info(Info).
+
+%% ===================================================================
+%% Private
+%% ===================================================================
+
+print_info(Info) ->
+    Info1 = [normalize(to_row(I)) || I <- Info],
+    ecli_tbl:print(Info1, 
+                   [
+                    {heads,[pid,name,val,current_function,initial_call]},
+                    {columns, [left,left,right,left,left]}
+                   ]).
+
+to_row({Pid, Val, [Name | Location]}) when is_atom(Name) ->
+    [{pid, Pid},{val, Val},{name, Name} | Location];
+to_row({Pid, Val, Location}) ->
+    [{pid, Pid},{val, Val},{name, <<>>} | Location].
+
+normalize(Vals) ->
+    [{K, ectl_lib:to_str(V)} || {K, V} <- Vals].
+
